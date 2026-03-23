5a2259a65a3328b2503f783eca01bd3e1529ba55
dengyao2004 <3060717303@qq.com>
2026-03-23 11:49:46 +0800

    初始化仓库项目

---
+++ src/main/java/com/gzu/enterprisemanager/EnterpriseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gzu.enterprisemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gzu/enterprisemanager/EnterpriseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzu.enterprisemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
